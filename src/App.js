467d987bfd47bcf649787167d6aa7572d17a2362
jitenderkumarproject <jitenderkumarprojects@gmail.com>
2024-07-20 13:15:20 +0530

    All Files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect } from "react";
+import * as Common from "./Common";
 function App() {
+  useEffect(() => {
+    Common.callApi("/api/server.php", ["fetch"], (result) => {
+      console.log(result);
+    });
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hey</h1>
     </div>
   );
 }
