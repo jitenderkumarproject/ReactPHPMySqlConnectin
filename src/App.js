55f00f3d54ee8d667d2df0b7ce8a1c8508ee21b7
jitenderkumarproject <jitenderkumarprojects@gmail.com>
2024-07-22 17:52:29 +0530

    CRUD with API Compleated

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
   const [designation, setDesignation] = useState("");
   const [address, setAddress] = useState("");
   const [eid, setEid] = useState("");
+  const [buttonstatus, setButtonStatus] = useState(false);
 
   useEffect(() => {
     Common.callApi("/api/server.php", ["fetch"], (result) => {
@@ -32,18 +33,37 @@ function App() {
     );
   }
 
-  // function updateData(){
-  //   Common.callApi("/api/server.php", ["update", "Jitendra", "IT Engineer", "Rajendra Place"], (result) => {
+  function updateData(eid, ename, designation, address) {
+    setButtonStatus(true);
+    setEid(eid);
+    setName(ename);
+    setDesignation(designation);
+    setAddress(address);
+  }
 
-  //   });
-  // }
-
-  function deleteData(Eid){
+  function deleteData(Eid) {
     setEid(Eid);
     Common.callApi("/api/server.php", ["delete", Eid], (result) => {
       setEmpData(JSON.parse(result));
     });
     //console.log("ID = " ,Eid);
+  }
+
+  function changeData() {
+    const obj = {
+      eid: eid,
+      ename: ename,
+      designation: designation,
+      address: address,
+    };
+    Common.callApi(
+      "/api/server.php",
+      ["update", JSON.stringify(obj)],
+      (result) => {
+        setEmpData(JSON.parse(result));
+        setButtonStatus(false);
+      }
+    );
   }
 
   return (
@@ -81,7 +101,11 @@ function App() {
           </td>
         </tr>
         <tr>
-          <button onClick={() => InsertData()}>Submit</button>
+          {buttonstatus ? (
+            <button onClick={changeData}> Update</button>
+          ) : (
+            <button onClick={() => InsertData()}>Submit</button>
+          )}
         </tr>
       </table>
 
@@ -103,7 +127,18 @@ function App() {
             <td>{item.EDesignation}</td>
             <td>{item.EAddress}</td>
             <td>
-              <button>Update</button>
+              <button
+                onClick={() =>
+                  updateData(
+                    item.Eid,
+                    item.EName,
+                    item.EDesignation,
+                    item.EAddress
+                  )
+                }
+              >
+                Update
+              </button>
             </td>
             <td>
               <button onClick={() => deleteData(item.Eid)}>Delete</button>
