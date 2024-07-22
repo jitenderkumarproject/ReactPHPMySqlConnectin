2737cb44c4bab27a17806039961c7730a385c564
jitenderkumarproject <jitenderkumarprojects@gmail.com>
2024-07-22 11:38:11 +0530

    Inserting Records

--- src/App.js
+++ src/App.js
@@ -1,16 +1,85 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import * as Common from "./Common";
+
 function App() {
+  const [empData, setEmpData] = useState([]);
   useEffect(() => {
     Common.callApi("/api/server.php", ["fetch"], (result) => {
-      console.log(result);
+      const resp = JSON.parse(result); //Data Goes to server in the form of String 
+      //so Parsing it in the form of JSON
+      setEmpData(resp); // setting the state
+       console.log(result);
+
     });
   }, []);
   return (
     <div className="App">
-      <h1>Hey</h1>
+
+    <table align="center" border={1}>
+    <th>Enter Employee Records</th>
+    <tr>
+      <td>Enter Name</td>
+      <td><input></input></td>
+    </tr>
+      <tr>
+      <td>
+        Enter Designation
+        
+      </td>
+      <td><input></input></td>
+      </tr>
+      <tr>
+        <td>
+          Enter Address
+        </td>
+        <td><input></input></td>
+      </tr>
+      <tr><button>Submit</button></tr>
+    </table>
+
+      <table align="center" border={1}>
+        <th>
+          <tr>
+            <td>
+             ID
+            </td>
+            <td>
+            Employee Name
+            </td>
+            <td>
+            Employee Designation
+            </td>
+            <td>
+            Employee Address
+            </td>
+            <td>
+            
+            </td>
+            <td>
+
+            </td>
+          </tr>
+        </th>
+        {
+           empData.map((item,i) =>
+           <tr>
+            <td>{item.Eid}</td>
+            <td>{item.EName}</td>
+            <td>{item.EDesignation}</td>
+            <td>{item.EAddress}</td>
+            <td>
+              <button>Update</button>
+             
+            </td>
+            <td>
+            <button>Delete</button>
+            </td>
+           </tr>
+           )
+        }
+      </table>
     </div>
   );
 }
